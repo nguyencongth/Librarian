9c6041f65d9306f24e2cdb90fec2bf02c92f9d53
Nguyễn Công Thành <thanhnc@dc1.gemvietnam.com>
2023-12-27 17:43:33 +0700

    UI login and navigation

--- librarian/src/app/app.component.ts
+++ librarian/src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './Layout/login/login.component';
+import { NavigationComponent } from './Layout/navigation/navigation.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule, 
+    RouterOutlet,
+    LoginComponent,
+    NavigationComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
